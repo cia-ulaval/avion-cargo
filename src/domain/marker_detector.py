b9ec68bdfb2d15fcc3d440c032d6fce04869792d
Bertrand Awenze <beawe@ulaval.ca>
2026-02-06 14:39:39 -0500

    feat: fin de l'implémentation du service de calibration

--- src/domain/marker_detector.py
+++ src/domain/marker_detector.py
@@ -3,14 +3,12 @@ from typing import List
 
 import numpy as np
 
-from domain.models import LandingTarget
+from domain.models import TargetedMarker
 
 
 class MarkerDetector(ABC):
     @abstractmethod
-    def detect(
-        self, frame: np.ndarray, target: LandingTarget
-    ) -> List[tuple[int, np.ndarray]]:
+    def detect(self, frame: np.ndarray, target: TargetedMarker) -> List[tuple[int, np.ndarray]]:
         """
         Returns list of (marker_id, corners).
         corners typically shape: (1,4,2) float32
