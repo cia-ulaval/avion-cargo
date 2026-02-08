87404f9e7778669774a9226999372e21b663ec28
Bertrand Awenze <beawe@ulaval.ca>
2026-02-07 22:46:11 -0500

    ref: reactivation de picamera

--- src/infrastructure/camera/picamera_adapter.py
+++ src/infrastructure/camera/picamera_adapter.py
@@ -1,26 +1,38 @@
+from typing import Any, Dict
+
 import numpy as np
 from picamera2 import Picamera2
 
 from domain.camera import Camera
 
+NUMBER_OF_MILLISECONDS_IN_ONE_SECOND = 1_000_000
+
 
 class PiCameraAdapter(Camera):
-    def __init__(self, width: int = 1280, height: int = 720, rgb: bool = True):
+    def __init__(self, fps: int, width: int = 1280, height: int = 720, rgb: bool = True):
         self._cam = Picamera2()
-        self._width = width
-        self._height = height
-        self._rgb = rgb
+        self._cam_config = self.__make_config(width=width, height=height, is_rgb_cam=rgb)
+        self._cam_controls = self.__make_controls(fps=fps)
         self._started = False
+
+    def __make_config(self, width: int, height: int, is_rgb_cam: bool) -> Dict[str, Any]:
+        config = {
+            "size": (width, height),
+            "format": "RGB888" if is_rgb_cam else "BGR888",
+        }
+
+        return config
+
+    def __make_controls(self, fps: int) -> Dict[str, Any]:
+        frame_duration = int(NUMBER_OF_MILLISECONDS_IN_ONE_SECOND / fps)
+        controls = {"FrameDurationsLimits": (frame_duration, frame_duration)}
+        return controls
 
     def open(self) -> None:
         if self._started:
             return
-        config = self._cam.create_preview_configuration(
-            main={
-                "size": (self._width, self._height),
-                "format": "RGB888" if self._rgb else "BGR888",
-            }
-        )
+
+        config = self._cam.create_preview_configuration(main=self._cam_config, controls=self._cam_controls)
         self._cam.configure(config)
         self._cam.start()
         self._started = True
