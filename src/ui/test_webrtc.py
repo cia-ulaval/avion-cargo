419add9ab4614e3fd5ba35a0e4fb196a7c0fd6b6
Bertrand Awenze <beawe@ulaval.ca>
2026-02-28 10:34:50 -0500

    chore: diffusion seulement avec la camera normal

--- src/ui/test_webrtc.py
+++ src/ui/test_webrtc.py
@@ -15,15 +15,15 @@ buf = LastestFrameBuffer()
 camera = OpenCVCamera()
 frame_processor = ArucoAxisAddingProcessor()
 target = TargetedMarker(marker_id=None, marker_length_m=0.181)
-calib_path = Path(
-    "/home/bertrand-awz/Documents/avionCargo/autolander/calibration_results/calibration_2026-02-07_21-45-24.npz"
-)
+# calib_path = Path(
+#     "/home/bertrand-awz/Documents/avionCargo/autolander/calibration_results/calibration_2026-02-07_21-45-24.npz"
+# )
 detector_config = OpenCVArucoDetectorConfig(dictionary_id=cv2.aruco.DICT_ARUCO_ORIGINAL)
-tracking_service = TrackingService.create(camera, target, calib_path, detector_config)
+# tracking_service = TrackingService.create(camera, target, calib_path, detector_config)
 
 
 webrtc = WebRTCContentDiffuser(buf, WebRTCConfig(port=8080, stream_fps=30))
-pipeline = ThreadedPipeline(camera=camera, frame_processor=tracking_service.track_once, frame_buffer=buf, com_channel=webrtc.diffuse_data)
+pipeline = ThreadedPipeline(camera=camera, frame_processor=camera.get_frame, frame_buffer=buf, com_channel=webrtc.diffuse_data)
 pipeline.start()
 
 webrtc.diffuse_video()  # bloque, ctrl-c pour arrêter
