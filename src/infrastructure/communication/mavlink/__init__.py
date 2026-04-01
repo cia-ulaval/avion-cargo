69f1b3fe73707cefd6a00fe372f923f0ab7d395c
Bertrand Awenze <beawe@ulaval.ca>
2026-03-31 20:49:52 -0400

    ref: formatage du code

---
+++ src/infrastructure/communication/mavlink/__init__.py
@@ -0,0 +1,5 @@
+""" """
+
+from .drone_mavlink_serial_connector import DroneMavlinkSerialConnector
+from .drone_mavlink_udp_connector import DroneMavlinkUDPConnector
+from .mavlink_connection_params import MavlinkConnectionParams
